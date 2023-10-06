475bd2541abd19f7700b9065a574c9e18333fafe
Aqeeb Rizwan <aqeeb.spider@gmail.com>
2023-10-06 16:30:20 +0530

    Initial commit

---
+++ Source/AlienInvasion/AlienInvasionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlienInvasionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIENINVASION_API AAlienInvasionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
